e5a534cabca7a91df6df5e668c522d5fa190a703
Stefan Misic <stefan.misic.491@gmail.com>
2018-05-12 12:02:11 +0200

    Add Semantic UI React

--- src/features/home/containers/HomeContainer.js
+++ src/features/home/containers/HomeContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 const HomeContainer = () => (
   <div>
     <h1>Hello, React!</h1>
+    <Button primary>Hello, Semantic UI!</Button>
   </div>
 );
 
